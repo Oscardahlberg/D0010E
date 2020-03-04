0a767a3d1bc7543f51ec50a40eb3a6b7bb41396f
gbgaltod <61691900+gbgaltod@users.noreply.github.com>
2020-03-04 20:12:41 +0100

    Update CustomerState.java

--- Labb5/src/State/CustomerState.java
+++ Labb5/src/State/CustomerState.java
@@ -4,7 +4,8 @@
 package State;
 
 /**
- * @author Jesper Frisk, jesfri-8
+ * All the different states a Customer can have.
+ * @author Jesper Frisk, Shahriar Chegini, Oscar Dahlberg, Folke Forshed.
  *
  */
 public enum CustomerState {
