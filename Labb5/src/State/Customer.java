71f3c7e1daf18448810c810668aaa5ee2012aeaa
gbgaltod <61691900+gbgaltod@users.noreply.github.com>
2020-03-04 20:11:44 +0100

    Update Customer.java

--- Labb5/src/State/Customer.java
+++ Labb5/src/State/Customer.java
@@ -1,32 +1,58 @@
 package State;
 
+/**
+ * The Customer class represents a customer.
+ * A customer haves a customer ID and a CustomerState.
+ * A customer can have four different states, 
+ * inStore, notInStore, turnedAway and lateCustomer.
+ * @author Jesper Frisk, Shahriar Chegini, Oscar Dahlberg, Folke Forshed.
+ *
+ */
 public class Customer 
 {
    private int customerID;
    private CustomerState state;
    
+   /**
+    * The Customer ID is set when created.
+    * @param customerNum
+    */
    public Customer(int customerNum)
    {
       this.customerID = customerNum;
    }
    
+   /**
+    * @return the customer ID
+    */
    public int getCustomerID()
    {
       return customerID;
    }
    
+   /**
+    * @return the customer state.
+    */
    public CustomerState getState()
    {
       return state;
    }
    
+   /**
+    * Sets the state of this customer.
+    * @param state
+    */
    public void setState(CustomerState state)
    {
       this.state = state;
    }
    
+   /**
+    * The toSting gives a sting with the customerID.
+    */
    public String toString()
    {
       return String.valueOf(customerID);
    }
 }
+
