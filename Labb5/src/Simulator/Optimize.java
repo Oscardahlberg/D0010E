43846cc41aee463785a9d6fe495b80e3dbc6dc71
gbgaltod <61691900+gbgaltod@users.noreply.github.com>
2020-03-04 20:15:37 +0100

    Update Optimize.java

--- Labb5/src/Simulator/Optimize.java
+++ Labb5/src/Simulator/Optimize.java
@@ -1,11 +1,30 @@
 package Simulator;
 
 import java.util.Random;
-
 import State.State;
 
+/**
+ * The class runs a set of simulations to find the most optimal number
+ * of registers while the other parameters a set.
+ * @author Jesper Frisk, Shahriar Chegini, Oscar Dahlberg, Folke Forshed.
+ *
+ */
 public class Optimize { 
 
+   /**
+    * Given all parameters a simulation is run and the number of missed
+    * customer is returned. 
+    * @param registers
+    * @param seed
+    * @param maxCustomers
+    * @param closingTime
+    * @param minPickTime
+    * @param maxPickTime
+    * @param minPayTime
+    * @param maxPayTime
+    * @param lambda
+    * @return the number of missed customers.
+    */
    public int metod1(int registers, long seed, int maxCustomers, 
          double closingTime, double minPickTime, double maxPickTime, 
          double minPayTime, double maxPayTime, double lambda)
@@ -19,8 +38,18 @@ public class Optimize {
       return state.getStore().getCustomersTurnedAway();
    }
    
+   /**
+    * Runs the first method until the best number of registers are found.
+    * It first tries with maxCustomer number of register because that gives 
+    * the minimal number of missed customers. The amount of registers are 
+    * lowered until one more customer is missed, then we know that the 
+    * last amount of registers was the best.
+    * @param seed
+    * @return best number of registers for this seed.
+    */
    public int metod2(long seed)
    {
+      // Set parameters for the first method.
       int maxCustomers = 5;
       double closingTime = 10;
       double minPickTime = 0.5;
@@ -29,15 +58,26 @@ public class Optimize {
       double maxPayTime = 3;
       double lambda = 1;
       
+      // Minimal registers can not be more than maxCustomers.
       int minRegisters = maxCustomers;
+      
+      // Gets an initial value of missedCustomers.
       int missedCustomers = metod1(minRegisters, seed, maxCustomers, 
             closingTime, minPickTime, maxPickTime, 
-            minPayTime, maxPayTime, lambda);;
+            minPayTime, maxPayTime, lambda);
+      
+      // Gets a new value as long as the minRegisters are bigger than 1.
       while(minRegisters >= 1)
       {
+         // Gets a new amount of missed customers.
          int newMissedCustomers = metod1(minRegisters, seed, maxCustomers, 
                closingTime, minPickTime, maxPickTime, 
                minPayTime, maxPayTime, lambda);
+         
+         // Checks if the new amount of missed customer is different from 
+         // from the initial value.
+         // if it is different that means that the last number of registers
+         // was the best amount.
          if(missedCustomers != newMissedCustomers)
          {
             return minRegisters + 1;
@@ -47,6 +87,14 @@ public class Optimize {
       return maxCustomers;
    }
    
+   /**
+    * Runs the second method with different seeds. We always save the 
+    * maximum of the minimum number of registers. The method stops tiring
+    * when the maximum has not changed for a hundred times.
+    * @param seed
+    * @return the maximum of the minimum number of registers with the 
+    * different seeds.
+    */
    public int metod3(long seed)
    {
       Random ran = new Random(seed);
@@ -72,7 +120,7 @@ public class Optimize {
    public static void main(String[] args) {
       Optimize op = new Optimize();
       long seed = -969854487314619770l;
-      System.out.println(op.metod2(Long.parseLong("1234")));
+      System.out.println(op.metod2(seed));
       
    }
 }
