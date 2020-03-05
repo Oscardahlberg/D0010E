c73d319997251f5cb1873af674d8725176289daa
kitkatShanky <61463459+kitkatShanky@users.noreply.github.com>
2020-03-05 09:11:26 +0100

    Update Start.java

--- Labb5/src/Event/Start.java
+++ Labb5/src/Event/Start.java
@@ -2,15 +2,33 @@ package Event;
 
 import State.*;
 
+/**
+ * 
+ * The Start event creates a number of arrival events and a close event
+ * by adding the events to the eventQueue. All the arrival events creates 
+ * at the same time. This event will be triggerd at the start of the 
+ * simulation and will allways be the first in eventQueue.
+ * 
+ * @author Jesper Frisk, Shahriar Chegini, Oscar Dahlberg, Folke Forshed.
+ *
+ */
+
 public class Start extends Event
 { 
+   private Close closing; 
+   private Arrival arrival;
    
    private double closeTime;
    private double time; 
    
-   private Close closing; 
-   private Arrival arrival;
-   
+  /**
+   * start will have State and EventQueue as argument so that we can 
+   * get access to the necessary variables and so that we can 
+   * add events to the eventQueue
+   * 
+   * @param state
+   * @param eventQueue
+   */
    public Start(State state, EventQueue eventQueue) 
    {
       super(state, eventQueue);
@@ -18,6 +36,16 @@ public class Start extends Event
       this.closeTime = state.getStore().getClosingTime();
    }
 
+   /**
+    * Firstly the method will "open" the store. 
+    * It will add close event to eventQueue so that we know when
+    * the store will close.
+    * All the arrival events will be created at the same time but  
+    * will be executed at different time. 
+    * Arrival time will be created if closeTime is greater than arrivalTime
+    * but the side effect will be that one person will be arriving to the 
+    * store after its closed.
+    */
    public void doMe() 
    {
       state.update(this);
@@ -40,17 +68,13 @@ public class Start extends Event
       return time;
    }
 
-   @Override
    public Customer getCustomer()
    {
-      // TODO Auto-generated method stub
       return null;
    }
 
-   @Override
    public String getName()
    {
-      // TODO Auto-generated method stub
       return "Start";
    }
 }
