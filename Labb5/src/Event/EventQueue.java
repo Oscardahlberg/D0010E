5b898e7caf25842c3a2edb9cfe83066072ae3de3
kitkatShanky <61463459+kitkatShanky@users.noreply.github.com>
2020-03-05 09:15:21 +0100

    Update EventQueue.java

--- Labb5/src/Event/EventQueue.java
+++ Labb5/src/Event/EventQueue.java
@@ -2,15 +2,17 @@ package Event;
 
 import java.util.ArrayList;
 
+/**
+ * In this class we will add and sort the events so that they 
+ * will be triggered at the right moment.
+ *
+ * @author Jesper Frisk, Shahriar Chegini, Oscar Dahlberg, Folke Forshed.
+ *
+ */
+
 public class EventQueue 
 {
    private ArrayList<Event> eventList = new ArrayList<Event>();
-   
-   public static void main(String[] args) 
-   {
-      EventQueue q = new EventQueue();
-      
-   }
    
    public int size()
    {
@@ -27,9 +29,19 @@ public class EventQueue
       eventList.remove(0);
    }
    
+   /**
+    * This method will add a new event and sort the queue,
+    * by compare the time that the different events have.
+    * It will first check all the events with lower execution
+    * time and add them to a new list and then it will put the 
+    * new event in the new list, lastly it will add all the events 
+    * with higher execution time to the new list and change the old list
+    * with the new one.
+    * s
+    * @param newEvent
+    */
    public void addEvent(Event newEvent) 
    {
-      //ArrayList<Integer> newIntList = new ArrayList<Integer>();
       ArrayList<Event> newEventList = new ArrayList<Event>();
       
       int counter = 0;
